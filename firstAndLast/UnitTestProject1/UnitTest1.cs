c7b805165bc2b1e98def844a9930e0e293cb1c2c
karashak1 <narlyman23@yahoo.com>
2013-08-29 17:46:47 -0400

    unitTest

---
+++ firstAndLast/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var json = firstAndLast.Program.GetProfile("jewpaltz");
+            Assert.IsNotNull(json);
+            Assert.IsTrue(json.Contains("Moshe"));
+        }
+    }
+}
