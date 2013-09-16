6ae969841f833e9d2a91f06d9142e1489af36a4e
karashak1 <karashak1@hawkmail.newpaltz.edu>
2013-09-16 17:43:41 -0400

    Linq where select from

--- firstAndLast/UnitTestProject1/VehicleUnitTest.cs
+++ firstAndLast/UnitTestProject1/VehicleUnitTest.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using firstAndLast;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace UnitTestProject1 {
     [TestClass]
@@ -47,5 +49,53 @@ namespace UnitTestProject1 {
             v.Transfrom();
             Assert.IsTrue(v.Log.Contains("autobot"));
         }
+
+        IEnumerable<int> numbers = Enumerable.Range(0, 100);
+        [TestMethod]
+        public void Ling101() {
+            //var numbers = Enumerable.Range(0,100)// new[] { 0, 1, 2, 3, 4, 5 };
+            var actual = new List<int>();
+            foreach (var x in numbers) {
+                if (x % 2 == 0)
+                    actual.Add(x);
+            }
+            Assert.AreEqual(50, actual.Count);
+            var actual2 = numbers.Where(y => y % 2 == 0);
+            //if(x % 2 == 0)
+            //numbers.
+            Assert.AreEqual(50, actual2.Count());
+
+            Assert.AreEqual(50, numbers.Count(y => y % 2 == 0));
+        }
+
+        [TestMethod]
+        public void LinqSelect() {
+            var actual = numbers.Select(x => x*x);
+            Assert.IsTrue(actual.Contains(9801));
+            var actual2 = numbers.Where(x => x % 3 == 0).Select(x => x * x);
+            Assert.IsTrue(actual2.Contains(9801));
+
+        }
+        
+        [TestMethod]
+        public void LinqComplex() {
+            var processess = System.Diagnostics.Process.GetProcesses();
+
+            var names = processess
+                .Select(x => new { x.ProcessName, ThreadCount = x.Threads.Count })
+                .Where(x => x.ThreadCount < 10);
+
+            var otherStuff = from x in processess 
+                             where x.Threads.Count > 3
+                             orderby x.ProcessName
+                             select new {x.ProcessName, x.PeakWorkingSet64, x.Responding };
+            
+            var otherStuff2 = from x in processess 
+                             where x.Modules.OfType<System.Diagnostics.ProcessModule>().Any(y => y.ModuleMemorySize > 1000)
+                             orderby x.ProcessName
+                             select new {x.ProcessName, x.PeakWorkingSet64, x.Responding };
+        
+        }
+         
     }
 }
