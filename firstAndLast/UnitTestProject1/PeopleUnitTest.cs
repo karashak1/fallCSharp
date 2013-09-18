5b331f8aad84ac7e86ee77cdf99401d43eafd145
karashak1 <karashak1@hawkmail.newpaltz.edu>
2013-09-17 22:52:49 -0400

    first homework lots of Linq

---
+++ firstAndLast/UnitTestProject1/PeopleUnitTest.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using firstAndLast;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace UnitTestProject1 {
+    [TestClass]
+    public class PeopleTests {
+
+        [TestMethod]
+        public void LoadNames() {
+            var names = new[] {"Bessie", "Vashti", "Frederica", "Nisha", "Kendall", "Magdalena", "Brendon"
+            , "Eve", "Manda", "Elvera", "Miquel", "Tyra", "Lucie", "Marvella", "Tracee", "Ramiro", "Irene", "Davina", "Jeromy" , "Siu"};
+            var people = from x in names
+                         select new People {
+                             Name = x,
+                         }; 
+            Assert.AreEqual(names.Count(), people.Count());
+        }
+
+        [TestMethod]
+        public void PeopleStartWithM() {
+            var names = new[] {"Bessie", "Vashti", "Frederica", "Nisha", "Kendall", "Magdalena", "Brendon"
+            , "Eve", "Manda", "Elvera", "Miquel", "Tyra", "Lucie", "Marvella", "Tracee", "Ramiro", "Irene", "Davina", "Jeromy" , "Siu"};
+            var people = from x in names
+                         select new People {
+                             Name = x,
+                         }; 
+            var peopleNameM = people.Where(x => x.Name.StartsWith("M"));
+            Assert.AreEqual(4, peopleNameM.Count());
+        }
+
+        [TestMethod]
+        public void PeoplesNamesToUppercase() {
+            var names = new[] {"Bessie", "Vashti", "Frederica", "Nisha", "Kendall", "Magdalena", "Brendon"
+            , "Eve", "Manda", "Elvera", "Miquel", "Tyra", "Lucie", "Marvella", "Tracee", "Ramiro", "Irene", "Davina", "Jeromy" , "Siu"};
+            var people = from x in names
+                         select new People {
+                             Name = x,
+                         };
+            var peopleUpperCase = from x in people
+                                  select new People {
+                                      Name = x.Name.ToUpper(),
+                                  };
+            Assert.AreEqual(names.Count(), peopleUpperCase.Count());
+
+        }
+
+        [TestMethod]
+        public void PeoplesNameLength() {
+            var names = new[] {"Bessie", "Vashti", "Frederica", "Nisha", "Kendall", "Magdalena", "Brendon"
+            , "Eve", "Manda", "Elvera", "Miquel", "Tyra", "Lucie", "Marvella", "Tracee", "Ramiro", "Irene", "Davina", "Jeromy" , "Siu"};
+            var people = from x in names
+                         select new People {
+                             Name = x,
+                         };
+            var peopleNameLength = people.Select(x => x.Name.Length).ToArray();
+            Assert.AreEqual(names.Count(), peopleNameLength.Count());
+        }
+    }
+}
