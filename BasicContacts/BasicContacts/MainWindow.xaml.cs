a4533f1a2ec9c17a620d1b5caeab99458606df74
karashak1 <karashak1@hawkmail.newpaltz.edu>
2013-10-10 17:48:24 -0400

    MVVM need to check his code for updates

--- BasicContacts/BasicContacts/MainWindow.xaml.cs
+++ BasicContacts/BasicContacts/MainWindow.xaml.cs
@@ -9,40 +9,22 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WpfApplication1 {
+namespace BasicContacts {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
-
-        string Team = "X";
         public MainWindow() {
             InitializeComponent();
+            DataContext = VM;
         }
-
+        TodoVM VM = new TodoVM();
         private void Button_Click(object sender, RoutedEventArgs e) {
-            var b = sender as TextBlock;
-            if (b == null) {
-
-            }
-            else {
-                if (Team == "X") {
-                    Team = "O";
-                    b.Background = Brushes.BlueViolet;
-                }
-                else {
-                    Team = "X";
-                    b.Background = Brushes.Pink;
-                }
-                b.Text = Team;
-                var s = Resources["Rotate"] as Storyboard;//windows resource get rotate cast to storyboard
-                s.Begin(b); //tell the storyboard to start
-            }
+            VM.List.Add(VM.Text);
         }
     }
 }
