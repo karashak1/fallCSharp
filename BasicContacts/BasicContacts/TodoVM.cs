df63678416badcb54140101059467396628c1b8d
karashak1 <karashak1@hawkmail.newpaltz.edu>
2013-10-10 21:31:29 -0400

    fixed the Tic tac toe and CRM

--- BasicContacts/BasicContacts/TodoVM.cs
+++ BasicContacts/BasicContacts/TodoVM.cs
@@ -1,10 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BasicContacts {
-    class TodoVM {
+    public class TodoVM : INotifyPropertyChanged{
+
+        
+        public string Text  { get; set; }
+        public ObservableCollection<string> List { get; set; }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public TodoVM() {
+            List = new ObservableCollection<string>();
+        }
     }
 }
