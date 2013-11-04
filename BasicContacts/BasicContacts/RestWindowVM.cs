7d3910bffc80eb7a952417daddcd9d2b3d3b90bd
karashak1 <karashak1@hawkmail.newpaltz.edu>
2013-11-04 17:46:49 -0500

    restAPI start but borked

---
+++ BasicContacts/BasicContacts/RestWindowVM.cs
@@ -0,0 +1,40 @@
+﻿using DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+
+namespace BasicContacts {
+    public class RestWindowVM : BaseVM{
+
+        private string _Log;
+
+        public string Log {
+            get { return _Log; }
+            set { _Log = value; OnPropertyChanged(); }
+        }
+
+        
+
+        public RestWindowVM() {
+            var client = new HttpClient();
+            //client.BaseAddress = new Uri("https://api.stackexchange.com/2.1/");
+            //Log = client.GetStringAsync("questions?site=stackoverflow").Result;
+            var formatter = new JsonMediaTypeFormatter();
+            client.BaseAddress = new Uri("https://graph.facebook.com");
+            var str = client.GetStringAsync("jewpaltz").Result;
+            var ob = Newtonsoft.Json.JsonConvert.DeserializeObject<FBuser>(str);
+            Log = ob.Name+ob.link + ob.id;
+
+        }
+    }
+
+    public class FBuser {
+        public string Name { get; set; }
+        public long id { get; set; }
+        public string link { get; set; }
+    }
+}
