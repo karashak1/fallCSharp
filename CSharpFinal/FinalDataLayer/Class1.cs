09c2fda985d0def58c111861dd54ce0833939d18
karashak1 <karashak1@hawkmail.newpaltz.edu>
2013-12-01 19:57:40 -0500

    final project
    it begins

---
+++ CSharpFinal/FinalDataLayer/Class1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace FinalDataLayer
+{
+
+    public class Address {
+        public int Id { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+    }
+
+    public class Company {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Address Address { get; set; }
+        public virtual ICollection<Contact> Employees { get; set; }
+    }
+
+    public class Contact {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int AddressID { get; set; }
+        public virtual Address Address { get; set; }
+        public int CompanyId { get; set; }
+        public virtual Company Company { get; set; }
+        public virtual ICollection<ContactMethod> ContactMethods { get; set; }
+        //public  MyProperty { get; set; }
+    }
+
+    public class ContactMethod {
+        public int Id { get; set; }
+        public string value { get; set; }
+        public string type { get; set; }
+        public int ContactId { get; set; }
+        public virtual Contact Contact { get; set; }
+    }
+
+    public class ContactsContext : DbContext {
+
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<ContactMethod> ContactMethods { get; set; }
+    }
+}
